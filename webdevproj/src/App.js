ee6afa40b5244c23da3226459204e1d223b296de
Mark Golden <g_olden7@hotmail.com>
2023-04-11 16:38:38 -0600

    working on connecting mongodb

--- webdevproj/src/App.js
+++ webdevproj/src/App.js
@@ -16,32 +16,32 @@ import Contact from './Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-    <div className="App">
-      <Header title="MERN WebDev Project"/>
-      <main className='main'>
-        <SideBar />
-        <div className='mainRouteDisplay'>
-          <Routes>
-            <Route exact path='/' element={<Home/>}></Route>
-            <Route exact path='/registration' element={<Registration/>}></Route>
-            <Route exact path='/authentication' element={<Authentication/>}></Route>
-            <Route exact path='/managedb' element={<ManageDB/>}></Route>
-            <Route exact path='/sellItem' element={<SellItem/>}></Route>
-            <Route exact path='/createList' element={<CreateList/>}></Route>
-            <Route exact path='/newsletter' element={<Newsletter/>}></Route>
-            <Route exact path='/changeTheme' element={<Theme/>}></Route>
-            <Route exact path='/profile' element={<Profile/>}></Route>
-            <Route exact path='/about' element={<About/>}></Route>
-            <Route exact path='/contact' element={<Contact/>}></Route>
-          </Routes>
+      return (
+        <Router>
+        <div className="App">
+          <Header title="MERN WebDev Project"/>
+          <main className='main'>
+            <SideBar />
+            <div className='mainRouteDisplay'>
+              <Routes>
+                <Route exact path='/' element={<Home/>}></Route>
+                <Route exact path='/registration' element={<Registration/>}></Route>
+                <Route exact path='/authentication' element={<Authentication/>}></Route>
+                <Route exact path='/managedb' element={<ManageDB/>}></Route>
+                <Route exact path='/sellItem' element={<SellItem/>}></Route>
+                <Route exact path='/createList' element={<CreateList/>}></Route>
+                <Route exact path='/newsletter' element={<Newsletter/>}></Route>
+                <Route exact path='/changeTheme' element={<Theme/>}></Route>
+                <Route exact path='/profile' element={<Profile/>}></Route>
+                <Route exact path='/about' element={<About/>}></Route>
+                <Route exact path='/contact' element={<Contact/>}></Route>
+              </Routes>
+            </div>
+          </main>
+          <Footer />
         </div>
-      </main>
-      <Footer />
-    </div>
-    </Router>
-  );
+        </Router>
+      );
 }
 
 export default App;
